262f407cf38121d60cb7973845a1c7c955ff9098
federikos <federikos@bk.ru>
2019-11-23 21:00:14 +0300

    add  load-more button

--- frontend/src/components/CatalogComponent/index.js
+++ frontend/src/components/CatalogComponent/index.js
@@ -3,16 +3,23 @@ import Cards from '../Cards';
 import PropTypes from 'prop-types';
 import Categories from './Categories';
 
-const getFetchUrl = id => {
-  if (!id) {
-    return `${process.env.REACT_APP_BASE_URL}items`;
-  }
-  return `${process.env.REACT_APP_BASE_URL}items?categoryId=${id}`
+const getFetchUrl = (id, offset, q) => {
+    let fetchUrl = `${process.env.REACT_APP_BASE_URL}items?offset=${offset}`;
+
+    if (id) {
+        fetchUrl += `&categoryId=${id}`
+    }
+
+    if (q) {
+        fetchUrl += `&q=${q}`
+    }
+
+  return fetchUrl
 }
 
-const fetchItems = (setLoading, id) => {
+const fetchItems = (setLoading, id, offset, q) => {
     setLoading(true);
-    const fetchUrl = getFetchUrl(id);
+    const fetchUrl = getFetchUrl(id, offset, q);
     return fetch(fetchUrl)
       .then(res => res.json())
       .then(res => res)
@@ -23,11 +30,34 @@ const CatalogComponent = ({search}) => {
   const [categoryId, setCategoryId] = useState(null);
   const [loading, setLoadig] = useState(false);
   const [items, setItems] = useState([]);
+  const [loadBtnVisible, setLoadBtnVisible] = useState(true);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
-    fetchItems(setLoadig, categoryId)
-        .then(res => setItems(res));
+    setLoadBtnVisible(true);
+
+    fetchItems(setLoadig, categoryId, 0, inputValue)
+        .then(async res => {
+            if (!res.length || res.length < 6) {
+                setLoadBtnVisible(false);
+            }
+            setItems(res);
+        })
   }, [categoryId]);
+
+  const handleLoadMore = () => {
+    fetchItems(setLoadig, categoryId, items.length, inputValue)
+    .then(res => {
+        if (!res.length || res.length < 6) {
+            setLoadBtnVisible(false);
+        }
+        setItems(prevItems => [...prevItems, ...res]);
+    });
+  }
+
+  const handleInputChange = e => {
+      setInputValue(e.target.value);
+  }
 
   return (
     <section className="catalog">
@@ -37,15 +67,19 @@ const CatalogComponent = ({search}) => {
           search
           &&
           <form className="catalog-search-form form-inline">
-            <input className="form-control" placeholder="Поиск" />
+            <input className="form-control" placeholder="Поиск" value={inputValue} onChange={handleInputChange} />
           </form>
         }
 
         <Categories setLoading={setLoadig} categoryId={categoryId} setCategoryId={setCategoryId} />
         <Cards loading={loading} items={items} isCatalog />
-        <div className="text-center">
-            <button className="btn btn-outline-primary">Загрузить ещё</button>
-        </div>
+        {
+            loadBtnVisible
+            &&
+            <div className="text-center">
+                <button className="btn btn-outline-primary" onClick={handleLoadMore}>Загрузить ещё</button>
+            </div>
+        }
     </section>
   );
 };
