719c5bbba12ac0f62bc13768da2379263454cb95
federikos <federikos@bk.ru>
2020-03-13 21:46:06 +0300

    try to fix

--- server/index.js
+++ server/index.js
@@ -117,7 +117,7 @@ router.post('/api/order', async (ctx, next) => {
 });
 
 router.get('*', function(ctx) {
-    return send(ctx, '../react-ui/build/index.html', {root: ctx.path});
+    return send(ctx, path.resolve('../react-ui/build/index.html'));
 });
 
 app.use(router.routes())
