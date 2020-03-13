e4798120d3c1441f39b2494593a8f30eddcb35dd
federikos <federikos@bk.ru>
2020-03-13 21:05:47 +0300

    try to comment the last response

--- server/index.js
+++ server/index.js
@@ -116,13 +116,13 @@ router.post('/api/order', async (ctx, next) => {
     return fortune(ctx, null, 204);
 });
 
-router.get('*', function(request, response) {
-    send(request, path.resolve(__dirname, '../react-ui/build', 'index.html'));
-});
+// router.get('*', function(request, response) {
+//     send(request, path.resolve(__dirname, '../react-ui/build', 'index.html'));
+// });
 
 app.use(router.routes())
 app.use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback());
-server.listen(port);
+server.listen(port);    
