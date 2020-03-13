9ef2c085d546510afe78320675e8d52f8f907584
federikos <federikos@bk.ru>
2020-03-13 16:59:19 +0300

    add build

---
+++ frontend/build/precache-manifest.c5c634847fbf23ffbceb434c80e1d0d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34200ea857bc7c8d4fb814db35c0216e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c68c27fb3c6d536ed5c5",
+    "url": "/static/css/main.4c15ff3a.chunk.css"
+  },
+  {
+    "revision": "5a83bbc00be4254571b7",
+    "url": "/static/js/2.6279a1ed.chunk.js"
+  },
+  {
+    "revision": "c68c27fb3c6d536ed5c5",
+    "url": "/static/js/main.50a2d648.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "/static/media/banner.d613d452.jpg"
+  },
+  {
+    "revision": "9c087d4352a142067a9686012eb7ba57",
+    "url": "/static/media/footer-sprite.9c087d43.png"
+  }
+]);
